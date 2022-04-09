2ce7a95a7f5e5322a579db6dc5bbf0759493153c
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-09 15:54:11 +0500

    10m

--- For10.py
+++ For10.py
@@ -1,4 +1,7 @@
 def main(list1):
+    r=[]
+    for x in list1:
+        r.append(x.capitalize())
     """
     Returns a list where only the first letter of each name is capitalized.
     Args:
@@ -6,4 +9,4 @@ def main(list1):
     Returns:
         list: return  answer
     """
-    return
+    return  r
